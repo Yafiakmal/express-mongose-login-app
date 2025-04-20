15c40a406a7f1c56b519a187245f90704632c079
Yafiakmal <yafiakmal45@gmail.com>
2025-04-20 17:41:19 +0700

    sixth commit

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,23 @@
-import app from '../src/app.js';
-import { createServer } from 'http';
+import express from 'express';
+import { connectDB } from '../src/db_config.js';
+import userRouter from '../src/routes/user.route.js';
 
-module.exports = (req: any, res: any) => {
-  const server = createServer(app);
-  server.emit('request', req, res);
-};
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+app.use('/api/users', userRouter);
+
+// 404 Middleware
+app.use((req, res) => {
+  res.status(404).json({ error: 'Route not found' });
+});
+
+// Error Middleware
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err.stack);
+  res.status(500).json({ error: 'Internal server error' });
+});
+
+export default app;
