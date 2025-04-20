c36daef370fe816932d94ee7f4a47b7dcf40e6b0
Yafiakmal <yafiakmal45@gmail.com>
2025-04-20 09:34:45 +0700

    third commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import morgan from 'morgan';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -10,25 +10,25 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
-app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the Express ES Module starter app!' });
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to the Express ES Module starter app!" });
 });
 
 app.use((req, res, next) => {
-  res.status(404).json({ error: 'Route not found' });
+  res.status(404).json({ error: "Route not found" });
 });
 
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).json({ error: 'Internal server error' });
+  res.status(500).json({ error: "Internal server error" });
 });
 
-if (process.env.NODE_ENV !== 'PRODUCTION') {
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running at http://localhost:${PORT}`);
-    });
-  }
-  
+if (process.env.NODE_ENV !== "PRODUCTION") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running at http://localhost:${PORT}`);
+  });
+}
+export default app;
