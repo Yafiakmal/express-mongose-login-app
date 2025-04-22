3fc50aefb00875f25a81ee2077fd212095608c74
Yafiakmal <yafiakmal45@gmail.com>
2025-04-22 13:06:24 +0700

    delete vercel configuration file

--- api/index.ts
+++ api/index.ts
@@ -3,17 +3,18 @@ import { connectDB } from "../src/models/db_config.js";
 import userRouter from "../src/routes/user.route.js";
 import authRouter from "../src/routes/auth.route.js";
 import { error } from "../src/label/error_label.js";
-import {
-  errorResponse,
-  successResponse,
-} from "../src/types/http_response.js";
-// await connectDB();
+import { errorResponse, successResponse } from "../src/types/http_response.js";
+await connectDB();
 
 const app = express();
 app.use(express.json());
 
-app.use('/api/auth', authRouter);
-app.use('/api/users', userRouter);
+app.get("/", (req, res) => {
+  res.send("http://localhost:3000/api/users");
+});
+
+app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
 
 // 404 Middleware
 app.use((req, res) => {
@@ -34,52 +35,6 @@ app.use(
 );
 
 
-
-
-// PERCOBAAN
-
-// import { error } from "../src/label/error_label.js";
-// import {
-//   TestInterface,
-//   ErrorResponse,
-//   SuccessResponse,
-// } from "../src/types/http_response.js";
-// console.log(error.INVALID_EMAIL_FORMAT);
-
-// function successResponse<T>(msg: string, data: T): SuccessResponse<T> {
-//   return {
-//     status: "success",
-//     message: msg,
-//     data: data,
-//   };
-// }
-
-// function errorResponse(msg: string, label: error): ErrorResponse {
-//   return {
-//     status: "error",
-//     label: label,
-//     message: msg,
-//   };
-// }
-
-// console.log(
-//   errorResponse("username atau password salah", error.EXISTING_CREDENTIALS)
-// );
-
-// console.log(
-//   successResponse("successfully registered", [
-//     {id:1, username:"Muhammad Yafi Akmal"},
-//     {id:2, username:"Budi Setiadi"}
-//   ])
-// )
-
-// END PERCOBAAN
-
-if (process.env.NODE_ENV !== "PRODUCTION") {
-  const port = process.env.PORT || 3000;
-  app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`);
-  });
-}
-
-export default app;
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server running at http://localhost:${process.env.PORT}`);
+});
--- src/models/db_config.ts
+++ src/models/db_config.ts
@@ -1,10 +1,13 @@
 import mongoose from "mongoose";
-import 'dotenv/config'; 
+import 'dotenv/config';
 
 export async function connectDB() {
   console.info("Connecting to database ...")
+  if (mongoose.connection.readyState === 1) {
+    console.info("MongoDB is already connected");
+    return;
+  }
   const MONGODB_URI = process.env.MONGODB_URI as string;
-  // console.log('MONGODB_URI:', process.env.MONGODB_URI);
 
   if(!MONGODB_URI){
     throw new Error("MONGODB_URI is not defined in environment variables.");
